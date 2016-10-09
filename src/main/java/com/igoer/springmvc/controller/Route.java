16721da5fe59da433e1d2e724bd039afbf766a3f
happiness <happinessaflower@msn.cn>
2016-10-09 17:12:35 +0800

    add route

--- src/main/java/com/igoer/springmvc/controller/Route.java
+++ src/main/java/com/igoer/springmvc/controller/Route.java
@@ -2,7 +2,12 @@ package com.igoer.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/9/30.
@@ -13,26 +18,204 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("route")
 public class Route {
 
+    /***************************************************
+     * 基本请求映射
+     ***************************************************/
+
     /**
-     * 通过返回 ModelAndView 跳转到 views 目录下的页面
+     * 通过返回 ModelAndView 跳转到 views/route 目录下的页面
      *
      * @return
      */
     @RequestMapping("/model")
-    public ModelAndView route_1() {
+    public ModelAndView model() {
         ModelAndView model = new ModelAndView();
-        model.setViewName("/route/route_1");
+        model.setViewName("route/route_1");
         return model;
     }
 
     /**
-     * 通过返回 String 跳转到 views 目录下对应页面
+     * 通过返回 String 跳转到 views/route 目录下对应页面
      *
      * @return
      */
     @RequestMapping("/string")
-    public String route_2() {
-        return "route_2";
+    public String string() {
+        return "route/route_2";
+    }
+
+    /**
+     * 重定向, 通过返回 ModelAndView 实现
+     *
+     * @return
+     */
+    @RequestMapping("/model302")
+    public ModelAndView model302() {
+        return new ModelAndView("redirect:model");
+    }
+
+    /**
+     * 重定向, 通过返回 String 实现
+     *
+     * @return
+     */
+    @RequestMapping("/string302")
+    public String string302() {
+        return "redirect:string";
+    }
+
+    /***************************************************
+     * Ant 风格请求映射
+     * ?    匹配一个字符
+     * *    匹配任意字符
+     * **   匹配多层路径（可以为 0 个）
+     ***************************************************/
+
+    /**
+     * 匹配如下请求地址
+     * /route/ant_1/a
+     * /route/ant_1/ab
+     * /route/ant_1/abc 等URL地址
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ant_1/*")
+    public ModelAndView ant_1() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 匹配如下请求地址
+     * /route/ant_2/a/index
+     * /route/ant_2/ab/index
+     * /route/ant_2/abc/index 等URL地址
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ant_2/*/index")
+    public ModelAndView ant_2() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 匹配如下请求地址
+     * /route/ant_3
+     * /route/ant_3/aa
+     * /route/ant_3/aa/bb
+     * /route/ant_3/aa/bb/cc 等URL地址
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ant_3/**")
+    public ModelAndView ant_3() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 匹配如下请求地址
+     * /route/ant_4/aa/index
+     * /route/ant_4/aa/bb/index
+     * /route/ant_4/aa/bb/cc/index 等URL地址
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ant_4/**/index")
+    public ModelAndView ant_4() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 匹配如下请求地址
+     * /route/ant_5aa
+     * /route/ant_5bb
+     * /route/ant_5cc 等URL地址
+     *
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ant_5??")
+    public ModelAndView ant_5() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 匹配如下请求地址
+     * /route/ant_6/a
+     * /route/ant_6/b
+     * /route/ant_6/c 等URL地址
+     *
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ant_6/?")
+    public ModelAndView ant_6() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /***************************************************
+     * 参数请求映射
+     ***************************************************/
+
+    /**
+     * 使用 @RequestMapping 的 method 属性设置请求方式
+     *
+     * @return
+     */
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public ModelAndView get() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 使用 @RequestMapping 的 params 属性设置
+     * 方法仅处理请求中包含了名为 “param”，值为 “123” 的请求
+     *
+     * @return
+     */
+    @RequestMapping(value = "params", params = "param=123")
+    public ModelAndView params() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 使用 @RequestMapping 的 header 属性设置 Reauest Header 中包含的属性
+     *
+     * 示例设置了只处理请求头中包含 MYHEADER 属性且值为 ABC123CDE
+     * 如无法匹配服务器返回 404
+     *
+     * @return
+     */
+    @RequestMapping(value = "/header", headers = "MYHEADER=ABC123CDE")
+    public ModelAndView header() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 使用 @RequestMapping 的 cousumes 属性设置
+     * 方法仅处理 request Content-Type 为 application/json 类型的请求
+     * 如无法匹配服务器返回 415
+     *
+     * @return
+     */
+    @RequestMapping(value = "/cousumes", consumes = "application/json")
+    public ModelAndView cousumes() {
+        return new ModelAndView("route/route_1");
+    }
+
+    /**
+     * 使用 @RequestMapping 的 produces 属性设置
+     * 方法仅处理 request Accept 为 application/json 类型的请求（浏览器支持的返回结果类型）
+     * 并且该请求将返回 application/json json类型数据
+     *
+     * @return
+     */
+    @RequestMapping(value = "produces", produces = "application/json")
+    @ResponseBody
+    public Map<String, String> produces() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("key1", "hello");
+        map.put("key2", "world");
+        return map;
     }
 
 }
